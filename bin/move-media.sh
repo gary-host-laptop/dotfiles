eecc77eb8c8abcc73ee6f402537f7e3832fc9cfa
gary-host-laptop <github.striven@aleeas.com>
2026-06-05 10:05:56 -0300

    replace path units with inotifywait service, fix move-media script

---
+++ bin/move-media.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+INBOX="$HOME/strata/10-19_system/16_inbox"
+SCREENSHOTS="$HOME/Pictures/Screenshots"
+IMAGES="$HOME/strata/30-39_media/33_images/images"
+SCREENSHOTS_DST="$HOME/strata/30-39_media/33_images/screenshots"
+
+inotifywait -m -e close_write --format '%w%f' \
+    "$INBOX" "$SCREENSHOTS" | while read -r filepath; do
+
+    filename=$(basename "$filepath")
+    dir=$(dirname "$filepath")
+
+    case "${filename,,}" in
+        *.jpg|*.jpeg|*.png|*.webp|*.gif)
+            if [ "$dir" = "$SCREENSHOTS" ]; then
+                mv "$filepath" "$SCREENSHOTS_DST/"
+            else
+                mv "$filepath" "$IMAGES/"
+            fi
+            ;;
+    esac
+done
