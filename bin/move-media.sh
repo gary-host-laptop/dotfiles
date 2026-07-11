50aedc4623ef1b1fb6788fdba7313767aa56cc5e
gary-host-laptop <github.striven@aleeas.com>
2026-07-11 20:18:39 -0300

    added lazygit, changed fastfetch logo and some ghostty config

--- bin/move-media.sh
+++ bin/move-media.sh
@@ -2,7 +2,7 @@
 
 INBOX="$HOME/strata/10-19_system/16_inbox"
 SCREENSHOTS="$HOME/Pictures/Screenshots"
-IMAGES="$HOME/strata/30-39_media/33_images/images"
+IMAGES="$HOME/strata/30-39_media/33_images/inbox"
 SCREENSHOTS_DST="$HOME/strata/30-39_media/33_images/screenshots"
 
 inotifywait -m -e close_write -e moved_to --format '%w%f' \
